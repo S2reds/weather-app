3391d2225e0829b803d1f45b0e9daec60f5ae145
S2reds <110210946+S2reds@users.noreply.github.com>
2023-03-17 22:23:44 -0700

    fixed api for bg & sizing issues

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import {requestAPI, requestGIF, requestBg} from './main.js'
+import './style.css'
 
 const formData = document.querySelector('form')
 const input = document.querySelector('input')
+const img = document.querySelector('#bg')
+const desc = document.querySelector('#desc')
+
 formData.addEventListener('submit', e => {
     e.preventDefault()
     if (!input.value) console.log('No location added')
     const data = Object.fromEntries(new FormData(formData).entries())
     const response = requestAPI(data.location)
-    response.then(res => console.log(res))
+    response.then(res => {
+        let bg = requestBg(res.weather[0].main)
+        desc.textContent = res.weather[0].main
+        bg.then(result => {
+            console.log(result)
+            img.src = result.results[0].urls.regular
+        })
+    })
     input.value = ''
 })
 
-
-
-JXWCSjOE6buim580jJqlMHvwJz4WtZVK24b1uAIgdg8
--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ async function requestAPI(location='Irvine') {
 async function requestGIF(val='cats') {
     const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=KCxJKhRuXqVWLLJbDfgdjvqw9BSFiv5C&s=${val}`)
     const json = await response.json()
-
+    return json
 }
 
 async function requestBg(val='clear') {
-    const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=JXWCSjOE6buim580jJqlMHvwJz4WtZVK24b1uAIgdg8&query=${val}`)
+    const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=JXWCSjOE6buim580jJqlMHvwJz4WtZVK24b1uAIgdg8&query=${val}&orientation=landscape`)
     const json = await response.json()
-
+    return json
 }
 
 export {requestAPI, requestGIF, requestBg}
