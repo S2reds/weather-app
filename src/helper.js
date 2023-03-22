437ec5c12e5e8c2bf00a80551e1b363a569f422e
S2reds <110210946+S2reds@users.noreply.github.com>
2023-03-21 18:36:33 -0700

    finished api inputs + styling & transitions

--- src/helper.js
+++ src/helper.js
@@ -11,7 +11,7 @@ async function requestGIF(val='cats') {
 }
 
 async function requestBg(val='forest') {
-    const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=JXWCSjOE6buim580jJqlMHvwJz4WtZVK24b1uAIgdg8&query=${val}&orientation=landscape`)
+    const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=JXWCSjOE6buim580jJqlMHvwJz4WtZVK24b1uAIgdg8&query=${val}&orientation=landscape&color=blue`)
     const json = await response.json()
     return json
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {requestAPI, requestGIF, requestBg} from './main.js'
+import {requestAPI, requestGIF, requestBg} from './helper.js'
 import './style.css'
 
 const formData = document.querySelector('form')
@@ -8,6 +8,14 @@ const desc = document.querySelector('#desc')
 const formDiv = document.querySelector('.inputDiv')
 const infoDiv = document.querySelector('.infoDiv')
 const gifDiv = document.querySelector('#gif')
+const name = document.querySelector('#locationName')
+const weather = document.querySelector('#locationWeather')
+const temp = document.querySelector('#locationTemp')
+const feelslike = document.querySelector('#feelslike')
+const humid = document.querySelector('#humid')
+const wind = document.querySelector('#wind')
+const box2 = document.querySelector('#box2')
+const sunsetter = document.querySelector('#sunset')
 
 window.addEventListener('load', e => {
     let bg = requestBg()
@@ -24,11 +32,24 @@ formData.addEventListener('submit', e => {
         console.log(res)
         gif = res.weather[0].main
         let bg = requestBg(res.weather[0].description)
-        desc.textContent = res.weather[0].description + ` @ ${data.location}`
+        name.textContent = res.name
+        weather.textContent = res.weather[0].description
+        temp.innerHTML = `${Math.floor(res.main.temp)}<sup>°F</sup>`
+        feelslike.innerHTML = `${Math.floor(res.main.feels_like)}<sup>°F</sup>`
+        humid.textContent = `${res.main.humidity}%`
+        wind.textContent = `${Math.ceil(res.wind.speed)} mph`
+        let sunset = new Date(res.sys.sunset * 1000)
+        let hours = sunset.getHours();
+        let minutes = '0' + sunset.getMinutes();
+        let seconds = '0' + sunset.getSeconds();
+        let time = hours + ":" + minutes.substr(-2) + ':' + seconds.substr(-2);
+        console.log(time)
+        console.log(sunset)
+        sunsetter.textContent = time
         bg.then(result => {
             console.log(result)
             img.src = result.results[0].urls.regular
-            let newGif = requestGIF(gif)
+            let newGif = requestGIF(gif+" weather")
             newGif.then(pic => {
                 console.log(pic)
                 gifDiv.src = pic.data.images.original.url
